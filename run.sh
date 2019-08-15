9c2b211c6f1e764af078645443db0494e0d57d8d
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2019-08-15 11:26:12 -0400

    remove data backfill code

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 python3 /code/manage.py migrate --noinput
-python3 /code/import_data.py
 python3 /code/push_cert_mailer_config.py
 python3 /code/push_cert_tools_config.py
 python3 /code/manage.py runserver 0.0.0.0:8000
