35598a15e4b74bfbd09969ba2b12cafbd411fb90
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2019-08-16 14:41:54 -0400

    model the mailer config
    closes #14

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@
 
 python3 /code/manage.py migrate --noinput
 python3 /code/import_data.py
-python3 /code/push_cert_mailer_config.py
 python3 /code/push_cert_tools_config.py
 python3 /code/manage.py runserver 0.0.0.0:8000
