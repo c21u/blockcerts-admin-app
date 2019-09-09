0be0066ec754e27b79fb8c395c10661ff8340da4
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2019-09-09 08:51:47 -0400

    allow aws healthchecks

--- admin_app/settings.py
+++ admin_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import sentry_sdk
+import requests
 from dotenv import load_dotenv
 from sentry_sdk.integrations.django import DjangoIntegration
 
@@ -168,3 +169,13 @@ SENTRY_DSN = os.getenv('SENTRY_DSN')
 sentry_sdk.init(
         dsn=SENTRY_DSN, integrations=[DjangoIntegration()]
 )
+
+# AWS Healthchecks
+EC2_PRIVATE_IP = None
+try:
+    EC2_PRIVATE_IP = requests.get('http://169.254.169.254/latest/meta-data/local-ipv4', timeout=0.01).text
+except requests.exceptions.RequestException:
+    pass
+
+if EC2_PRIVATE_IP:
+    ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
