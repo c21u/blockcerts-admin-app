c5b9f4cebcc45e1d2cc72140458d94bced4de1b8
Emily Reese <eareese@eareese.com>
2019-09-20 10:52:49 -0400

    Refactor login command.

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -1,38 +1,21 @@
 const casServerUrl = "https://casserver.herokuapp.com/cas/login";
 
 Cypress.Commands.add("login", () => {
-  const firstRequest = {
-    method: "GET",
-    url: casServerUrl
-  };
-
-  const secondRequest = {
-    method: "POST",
-    url: casServerUrl,
-    qs: {
-      service:
-        "http://admin.127.0.0.1.xip.io/accounts/login?next=%2Fmanage_credentials%2F"
-    },
-    followRedirect: true,
-    form: true,
-    body: {
-      username: "casuser",
-      password: "Mellon",
-      _eventId: "submit"
-    }
-  };
-
-  cy.request(firstRequest)
-    .then(response => {
-      expect(response.status).to.eq(200);
-
-      const parser = new DOMParser();
-      const html = parser.parseFromString(response.body, "text/html");
-      const inputElements = html.querySelectorAll("input[name='execution']");
-
-      if (inputElements && inputElements[0] && inputElements[0]["value"]) {
-        secondRequest.body.execution = inputElements[0]["value"];
+  cy.request(casServerUrl).then(response => {
+    const parser = new DOMParser();
+    const html = parser.parseFromString(response.body, "text/html");
+    const inputElement = html.querySelector("input[name='execution']");
+    expect(inputElement).to.not.be.null;
+    cy.request({
+      method: "POST",
+      url: casServerUrl,
+      form: true,
+      body: {
+        execution: inputElement["value"],
+        username: "casuser",
+        password: "Mellon",
+        _eventId: "submit"
       }
-    })
-    .then(() => cy.request(secondRequest))
+    });
+  });
 });
