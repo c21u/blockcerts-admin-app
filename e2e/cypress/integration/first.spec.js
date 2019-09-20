39a1daeb81371d1edcd9c2ecb69dd71a083c932c
Emily Reese <eareese@eareese.com>
2019-09-20 10:35:58 -0400

    Specify element test IDs.

--- e2e/cypress/integration/first.spec.js
+++ e2e/cypress/integration/first.spec.js
@@ -2,7 +2,7 @@ describe("Basic UI", () => {
   before(() => {
     // log in only once before any of the tests run.
     cy.login();
-  })
+  });
   beforeEach(() => {
     // before each test, we can automatically preserve certain cookies,
     // so they will not be cleared before the next test starts.
@@ -11,16 +11,18 @@ describe("Basic UI", () => {
 
   it("Should find form element at /add_credential", () => {
     cy.visit("/add_credential");
-    cy.get("form").should("exist");
+    cy.get("form[data-test-id=app-credential-form]").should("exist");
   });
 
   it("Should find form element at /add_issuance", () => {
     cy.visit("/add_issuance");
-    cy.get("form").should("exist");
+    cy.get("form[data-test-id=app-issuance-form]").should("exist");
   });
 
   it("Should find view title at /manage_credentials", () => {
-    cy.visit("/manage_credentials")
-    cy.get("h1").should("exist").should("contain", "Manage Credentials");
+    cy.visit("/manage_credentials");
+    cy.get("h1[data-test-id=app-credential-manage-title]")
+      .should("exist")
+      .should("contain", "Manage Credentials");
   });
 });
