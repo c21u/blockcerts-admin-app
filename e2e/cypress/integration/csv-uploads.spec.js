3ab180cb21385f7cf06b4d5d279158673fe99b78
Emily Reese <eareese@eareese.com>
2019-09-28 12:30:53 -0400

    Handle BOM in csv upload via utf-8-sig encoding.
    Use utf-8-sig encoding to auto handle BOMs.
    Add a test and fixture for it.

--- e2e/cypress/integration/csv-uploads.spec.js
+++ e2e/cypress/integration/csv-uploads.spec.js
@@ -28,4 +28,28 @@ describe("CSV Uploads", () => {
       .should("exist")
       .should("contain", "Manage Recipients - Approve");
   });
+
+  it("Should not blow up when a CSV with BOM is uploaded", () => {
+    cy.visit("/manage_recipients/1/invite");
+    cy.get("h1[data-test-id=app-credential-invite-title]")
+      .should("exist")
+      .should("contain", "Manage Recipients - Invite");
+
+    const fileName = "another.csv";
+    cy.fixture(fileName).then(fileContent => {
+      console.dir(fileContent);
+      cy.get("#csv_file").upload({
+        fileContent,
+        fileName: fileName,
+        mimeType: "text/csv"
+      });
+    });
+
+    cy.get("button[data-test-id=app-invite-submit-button]").click();
+
+    // expect successful submit to redirect to Manage Recipients - Approve view
+    cy.get("h1[data-test-id=app-credential-approve-title]")
+      .should("exist")
+      .should("contain", "Manage Recipients - Approve");
+  })
 });
