a88b5b8060094b85009989fcfe9fd9320b626f7a
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2019-10-03 15:55:47 -0400

    add test for creating a credential

---
+++ e2e/cypress/integration/credential.spec.js
@@ -0,0 +1,34 @@
+describe("Credential", () => {
+  context("Create Credential custom command", () => {
+    const CREATE_CREDENTIAL = "createCredential";
+    Cypress.Commands.add(CREATE_CREDENTIAL, fixtureName => {
+      cy.visit("/add_credential");
+      cy.get("#id_title").type("Underwater Basketweaving 101");
+      cy.get("#id_description").type("Intro to Underwater Basketweaving");
+      cy.get("#id_narrative").type("Demonstrated ability to weave a basket underwater.");
+      cy.get("#id_issuing_department").type("College of Reed and Vine Based Arts");
+      cy.get("#id_cert_mailer_config").select("1");
+      cy.get("#id_cert_tools_config").select("1");
+
+      cy.get(".btn-primary").click();
+
+      // expect successful submit to redirect to same page with empty fields
+      cy.location('href').should('include', '/add_credential')
+      cy.get("#id_title").invoke('val').should('eq', '');
+      cy.get("#id_description").invoke('val').should('eq', '');
+      cy.get("#id_narrative").invoke('val').should('eq', '');
+      cy.get("#id_issuing_department").invoke('val').should('eq', '');
+
+    });
+  });
+
+  before(() => {
+    // log in only once before any of the tests run.
+    cy.login();
+  });
+
+  // NB: this test is gonna be flaky since it relies on a seeded fixture with ID=1
+  it("Should create a credential", () => {
+    cy.createCredential();
+  });
+});
